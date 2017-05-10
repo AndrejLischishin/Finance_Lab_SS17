35bb324078d566935c6eeb777db2a5b840934cdd
HenKlei <hendrik97@gmx.net>
2017-05-10 21:57:28 +0200

    Removed my folder

--- Hendrik/Worksheet1/html/search/all_2.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['normal_5fcdf',['normal_CDF',['../random__functions_8hpp.html#ac420155805f91f3b94cba4b9980056f5',1,'random_functions.hpp']]],
-  ['normal_5ffrom_5fstandard_5fnormal_5fdistribution',['normal_from_standard_normal_distribution',['../random__functions_8hpp.html#a7e15cd7602557cc75c75c192131e9f0f',1,'random_functions.hpp']]],
-  ['normal_5finverse_5fcdf',['normal_inverse_CDF',['../random__functions_8hpp.html#a60c5de2cc82852221db4885a946838c5',1,'random_functions.hpp']]]
-];
--- Hendrik/Worksheet1/html/search/functions_3.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['random_5fnumber_5f01',['random_number_01',['../random__functions_8hpp.html#a19526e6fadf775a56735c2ba7dde80b9',1,'random_functions.hpp']]],
-  ['random_5fnumber_5f01_5fgsl',['random_number_01_GSL',['../random__functions_8hpp.html#a955b51c233f8992897ca5d6497d68c31',1,'random_functions.hpp']]],
-  ['random_5fnumber_5fab_5fgsl',['random_number_ab_GSL',['../random__functions_8hpp.html#ac18cf0ecb32c64011f8d525a5279013e',1,'random_functions.hpp']]],
-  ['rejection_5fsampling',['rejection_sampling',['../random__functions_8hpp.html#a48c7e3ff5be4836729f9cdabb160d105',1,'random_functions.hpp']]]
-];
