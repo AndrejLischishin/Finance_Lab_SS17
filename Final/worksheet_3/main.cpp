d08f0555fffe66194976b6b0ac3b1ae059d1d981
WhoAmI <WhoAmI@pptp-212-201-75-41.pptp.stw-bonn.de>
2017-05-25 19:20:43 +0200

    worksheet_3 ready to start

---
+++ Final/worksheet_3/main.cpp
@@ -0,0 +1,32 @@
+//
+//  main.cpp
+//
+//  Created by WhoAmI on 28.04.17.
+//  Copyright © 2017 Andrei. All rights reserved.
+//
+/** @file */
+#include "../header_files/random_functions.hpp"
+#include "../header_files/simulation_functions.hpp"
+#include "../header_files/integration_functions.hpp"
+//plot with Python in C++ programs
+//#include "../header_files/matplotlibcpp.hpp"
+
+/**
+ * Main function to run all exercises of worksheet 2.
+ *
+ * @param argc Integer argument for main
+ * @param argv Char array argument for main
+ *
+ * @return Returns 0 if everything worked fine
+ *
+ */
+
+//Plot, but because with Python and system depending, comented
+//namespace plt = matplotlibcpp;
+
+
+int main(int argc, char* argv[]){
+  std::cout << "Prepared everything for worksheet 3." << std::endl;
+
+	return 0;
+}
