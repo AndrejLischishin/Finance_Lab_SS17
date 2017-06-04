ce27005df0086deb6513160a6ad801c71bacc363
WhoAmI <WhoAmI@pptp-212-201-70-197.pptp.stw-bonn.de>
2017-06-04 11:04:50 +0200

    error in clenshaw curtis found an corrected

--- Final/worksheet_2/main.cpp
+++ Final/worksheet_2/main.cpp
@@ -408,12 +408,24 @@ int main(int argc, char* argv[])
 		myfile<<calculate_relative_error(exact_result, calculated_result)<<"	";
 
 		/* Clenshaw Curtis */
+
 		nodes->clear();
 		weights->clear();
 		clenshaw_curtis(nodes, weights, l);
 		calculated_result = integrate_by_point_evaluation(call_option_integrand, nodes, weights, pow(2,l)-1, Task_10::s0, Task_10::mu, Task_10::sigma, Task_10::T, Task_10::K);
 		//std::cout << calculated_result << std::endl;
 		myfile<<calculate_relative_error(exact_result, calculated_result)<<"	";
+
+        double sum = 0;
+
+
+        //check if sum of the weights is 1
+        for (int i = 0; i<pow(2,l)-1;i++ ) {
+
+            sum += (*weights)[i];
+        }
+
+        std::cout<<"sum: "<<sum<<std::endl;
 
 		/* Gauss Legendre */
 		nodes->clear();
