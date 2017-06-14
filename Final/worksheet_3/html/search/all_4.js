fdf261adbab97b6bc09ee1cd8b750731a7bb79b9
WhoAmI <WhoAmI@vpn-211-30.wlan.uni-bonn.de>
2017-06-14 16:54:18 +0200

    update

--- Final/worksheet_3/html/search/all_4.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['integrate_5fby_5fpoint_5fevaluation',['integrate_by_point_evaluation',['../integration__functions_8cpp.html#ab4e5885b5cf28ff09b4cfbf603dfcb85',1,'integration_functions.cpp']]],
-  ['integration_5ffunctions_2ecpp',['integration_functions.cpp',['../integration__functions_8cpp.html',1,'']]]
-];
