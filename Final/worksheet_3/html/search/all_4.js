f56d64271586d6cd2aa8a98d11437cc6e1165629
HenKlei <hendrik97@gmx.net>
2017-06-19 20:40:21 +0200

    Finished Task 13 and added plots in PDF file

--- Final/worksheet_3/html/search/all_4.js
+++ Final/worksheet_3/html/search/all_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['integrate_5fby_5fpoint_5fevaluation',['integrate_by_point_evaluation',['../integration__functions_8cpp.html#ab4e5885b5cf28ff09b4cfbf603dfcb85',1,'integration_functions.cpp']]],
+  ['integrate_5fby_5fpoint_5fevaluation',['integrate_by_point_evaluation',['../integration__functions_8cpp.html#a399d5e5451131f462dc2cd2cdd77c6f7',1,'integration_functions.cpp']]],
   ['integration_5ffunctions_2ecpp',['integration_functions.cpp',['../integration__functions_8cpp.html',1,'']]]
 ];
